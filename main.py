a998f0a5e3a699ea984f3369f8c2111be85df7e2
Youssef Ahmad Abouhemid <youssefabouhemid@gmail.com>
2024-10-01 20:58:43 +0300

    add dns resolver

---
+++ main.py
@@ -0,0 +1,13 @@
+import socket
+
+def resolve_nginx():
+    try:
+        nginx_service = "nginx.default.svc.cluster.local"
+        print(f"Resolving {nginx_service}")
+        ip = socket.gethostbyname(nginx_service)
+        print(f"Nginx Service IP: {ip}")
+    except socket.gaierror:
+        print("Error resolving the DNS")
+
+if __name__ == "__main__":
+    resolve_nginx()
