f4ce52f2c68b5eb2aa9fb29c6d7032f836aaa2ff
Youssef Ahmad Abouhemid <63516733+greatyassoo@users.noreply.github.com>
2024-10-07 03:10:54 +0300

    add ec2 ecr access IAM role

--- terraform/ec2_instances.tf
+++ terraform/ec2_instances.tf
@@ -10,12 +10,64 @@ data "aws_ami" "ubuntu" {
 }
 
 
+# IAM Role for EC2 instances
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "ecr_policy" {
+  name = "ecr_policy"
+  role = aws_iam_role.ec2_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:PutImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# IAM Instance Profile
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "ec2_profile"
+  role = aws_iam_role.ec2_role.name
+}
+
+
 resource "aws_instance" "k3s_instance" {
   ami                     = data.aws_ami.ubuntu.id  
   instance_type           = var.instance_type
   subnet_id               = aws_subnet.public.id  # Reference the public subnet directly
   vpc_security_group_ids  = [aws_security_group.k3s_sg.id]  # Reference the created SG directly
   key_name                = aws_key_pair.terraform_key.key_name # Reference the created key in data
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
+
 
   associate_public_ip_address = true
   
@@ -32,6 +84,8 @@ resource "aws_instance" "jenkins_instance" {
   key_name              = aws_key_pair.terraform_key.key_name
   subnet_id             = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.k3s_sg.id]   # Reference the created SG directly
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
+
 
   associate_public_ip_address = true
 
